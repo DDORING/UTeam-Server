29e5b55b8d6936fa2152b21dfbab99c33652ecb2
yumzen <yeom.ji.eun35@gmail.com>
2024-06-01 22:54:53 +0900

    Feat: DopamineTime Entity 생성

---
+++ core/src/main/java/com/api/core/domain/dopamineTime/entity/DopamineTime.java
@@ -0,0 +1,41 @@
+package com.api.core.domain.dopamineTime.entity;
+
+import com.api.core.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DopamineTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long doptime_id;
+
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.EAGER)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @Column(name="is_finished", nullable = false, columnDefinition =  "tinyint")
+    private boolean isFinished;
+
+    @Column(name="is_extended", nullable = false, columnDefinition =  "tinyint")
+    private boolean isExtended;
+
+    @Column(name = "end_time", nullable = true, columnDefinition = "timestamp")
+    private LocalDateTime endTime;
+
+    @Builder
+    public DopamineTime(Member member, boolean isExtended, boolean isFinished, LocalDateTime endTime){
+        this.member = member;
+        this.isExtended = isExtended;
+        this.isFinished = isFinished;
+        this.endTime = endTime;
+    }
+}
