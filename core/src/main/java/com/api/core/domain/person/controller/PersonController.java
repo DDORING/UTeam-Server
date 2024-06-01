4da35f2791459aa9ad03edf99fa7773dac3a1b1e
minjgziii <kmj4668@gmail.com>
2024-06-02 05:02:03 +0900

    feature/person : swagger관련 @Operation 추가

--- core/src/main/java/com/api/core/domain/person/controller/PersonController.java
+++ core/src/main/java/com/api/core/domain/person/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.api.core.domain.person.controller;
 import com.api.core.domain.person.dto.PersonDto;
 import com.api.core.domain.person.entity.Person;
 import com.api.core.domain.person.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/person")
+@Tag(name = "Person", description = "사용자가 선택한 인물(연예인 등) 관련 API")
 public class PersonController {
 
     private final PersonService personService;
@@ -20,6 +23,7 @@ public class PersonController {
     }
 
     @PostMapping("/upload/{memberId}")
+    @Operation(summary = "인물 생성 및 수정 API", description = "인물의 이름 및 이미지 생성")
     public ResponseEntity<PersonDto> uploadPersonInfo(
             @PathVariable Long memberId,
             @RequestParam("name") String name,
@@ -36,6 +40,7 @@ public class PersonController {
 
     // 인물 조회
     @GetMapping("/get/{memberId}")
+    @Operation(summary = "인물 조회 API", description = "인물의 이름 및 이미지 조회")
     public ResponseEntity<PersonDto> getFavoritePerson(@PathVariable Long memberId) {
         return personService.findPerson(memberId)
                 .map(person -> new PersonDto(person.getName(), person.getPhoto()))
