be923df70bd4eaf04d586d7637601dcf39834b44
minjgziii <kmj4668@gmail.com>
2024-06-02 02:22:43 +0900

    feature/person : Person dto 작성

---
+++ core/src/main/java/com/api/core/domain/person/dto/PersonDto.java
@@ -0,0 +1,28 @@
+package com.api.core.domain.person.dto;
+
+public class PersonDto {
+
+    private String name;
+    private String photo;
+
+    public PersonDto(String name, String photo) {
+        this.name = name;
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
