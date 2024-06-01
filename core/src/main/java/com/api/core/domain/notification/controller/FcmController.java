65575ade0e182622d8d848a699ec26e1e404e16b
yumzen <yeom.ji.eun35@gmail.com>
2024-06-02 07:25:55 +0900

    Fix: FirebaseCloudMessage 오류 수정

--- core/src/main/java/com/api/core/domain/notification/controller/FcmController.java
+++ core/src/main/java/com/api/core/domain/notification/controller/FcmController.java
@@ -2,10 +2,8 @@ package com.api.core.domain.notification.controller;
 
 import com.api.core.domain.notification.dto.FcmSendDto;
 import com.api.core.domain.notification.service.FcmService;
-import com.google.firebase.messaging.Message;
+import com.api.core.global.common.ApplicationResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +12,7 @@ import java.io.IOException;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/fcm")
+@RequestMapping("fcm")
 public class FcmController {
 
     private final FcmService fcmService;
@@ -23,11 +21,9 @@ public class FcmController {
         this.fcmService = fcmService;
     }
 
-    @PostMapping("/send")
-    public ResponseEntity<HttpStatus> pushMessage(@RequestBody @Validated FcmSendDto fcmSendDto) throws IOException {
-        log.debug("[+] 푸시 메시지를 전송합니다. ");
-        int result = fcmService.sendMessageTo(fcmSendDto);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("")
+    public ApplicationResponse<?> pushMessage(@RequestBody @Validated FcmSendDto fcmSendDto) throws IOException {
+        return ApplicationResponse.ok(fcmService.sendMessageTo(fcmSendDto));
     }
 
 }
